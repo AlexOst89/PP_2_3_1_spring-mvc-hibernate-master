124e3c62f7fe6999c17c98e6c58419692f7a09ef
Alexandr Ostapenko <ostap.sanechek@gmail.com>
2025-07-30 19:47:44 +0300

    2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,6 +3,7 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
@@ -10,6 +11,7 @@ import web.service.CarService;
 import java.util.List;
 
 @Controller
+@RequestMapping("/cars")
 public class CarController {
 
     private final CarService carService;
@@ -18,10 +20,9 @@ public class CarController {
         this.carService = carService;
     }
 
-    @GetMapping("/cars")
+    @GetMapping
     public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        List<Car> cars = carService.getCars(count);
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
